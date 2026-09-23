8af6e23948e7694795ce7d81029e2518c1a26aec
Toni <tonir1695@gmail.com>
2026-09-23 16:05:17 +0200

    Delete UtilisateurRepository.java

--- UtilisateurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.santerdv.santerdvapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
-    Optional<Utilisateur> findByEmail(String email);
-    java.util.List<Utilisateur> findByRole(String role);
-}
