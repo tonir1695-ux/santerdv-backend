146cb28efc95aa756c44a65e38355ad3e8e6b365
Toni <tonir1695@gmail.com>
2026-09-23 15:56:57 +0200

    Delete RendezVousRepository.java

--- RendezVousRepository.java
+++
@@ -1,9 +0,0 @@
-package com.santerdv.santerdvapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RendezVousRepository extends JpaRepository<RendezVous, Integer> {
-    List<RendezVous> findByPatientId(Integer idPatient);
-    List<RendezVous> findByMedecinId(Integer idMedecin);
-}
